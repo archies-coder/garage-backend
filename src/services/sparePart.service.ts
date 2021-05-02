aed7b374e18cdc2adb37101245f1622530fd3b64
harshal <harshalsgore@gmail.com>
2021-05-02 11:55:24 +0530

    quantity added

--- src/services/sparePart.service.ts
+++ src/services/sparePart.service.ts
@@ -8,15 +8,17 @@ const fetchAll = async () => {
 const createNewSparePart = async (sparePart: ISparePartDTO) => {
   const data = await SparePartModel.find()
   let value = true
+  let add = 0
   data.map(item => {
     if (item.name === sparePart.name && item.category === sparePart.category) {
       value = false
+      add = item.quantity
     }
   })
   if (value) return SparePartModel.create(sparePart)
   else {
     const filter = { name: sparePart.name }
-    const update = { quantity: sparePart.quantity }
+    const update = { quantity: add + sparePart.quantity }
     return SparePartModel.findOneAndUpdate(filter, update, { new: true })
   }
 }
