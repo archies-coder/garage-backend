1db640eeeaaf2c796c7f04218c6e310859aabd88
Archis Bhoir <archisbhoir2016@gmail.com>
2021-05-04 13:17:48 +0530

    stats by date

--- src/controllers/stats.controller.ts
+++ src/controllers/stats.controller.ts
@@ -2,9 +2,11 @@ import { NextFunction, Request, Response } from 'express'
 import { fetchAll } from '../services/stats.service'
 
 const getStatsData = async (req: Request, res: Response, next: NextFunction) => {
-  const getData = await fetchAll()
+  const date = req.query.date
+  let data
+  date ? (data = await fetchAll(date)) : await fetchAll()
   res.send({
-    data: getData,
+    data: data,
   })
 }
 
--- src/services/stats.service.ts
+++ src/services/stats.service.ts
@@ -1,10 +1,21 @@
 import vehicleEntryModel from '../models/vehicleEntry.model'
 import { subDays, addDays } from 'date-fns'
 
-const fetchAll = async () => {
-  const now = new Date()
-  const startOfToday = new Date(now.getFullYear(), now.getMonth(), now.getDate())
+interface IStat {
+  delivered: number
+  in_garage: number
+  parts_used: number
+  total_customers: number
+  vehicleEntriestats: number[]
+}
 
+const fetchAll = async (date?: any) => {
+  const referenceDate = date ? new Date(date) : new Date()
+  const startOfDate = new Date(
+    referenceDate.getFullYear(),
+    referenceDate.getMonth(),
+    referenceDate.getDate(),
+  )
   const days = [
     new Date(subDays(new Date(), 0).setUTCHours(0, 0, 0, 0)).toISOString(),
     new Date(subDays(new Date(), 1).setUTCHours(0, 0, 0, 0)).toISOString(),
@@ -12,23 +23,38 @@ const fetchAll = async () => {
     new Date(subDays(new Date(), 3).setUTCHours(0, 0, 0, 0)).toISOString(),
     new Date(subDays(new Date(), 4).setUTCHours(0, 0, 0, 0)).toISOString(),
   ]
-  const visitorsByDay: any = []
+  const vehicleEntriesByDay: number[] = []
   days.map(async (element, i) => {
-    visitorsByDay[i] = await vehicleEntryModel.countDocuments({
-      createdAt: {
-        $gte: element,
-        $lt: addDays(new Date(element), 1).toISOString(),
-      },
-    })
+    vehicleEntriesByDay[i] =
+      (await vehicleEntryModel.countDocuments({
+        createdAt: {
+          $gte: element,
+          $lt: addDays(new Date(element), 1).toISOString(),
+        },
+      })) || 0
   })
 
-  const data: any = {}
-  data.delivered = (await vehicleEntryModel.countDocuments({ outime: { $gte: startOfToday.toISOString() } })) || 0
+  const data: IStat = {
+    delivered: 0,
+    in_garage: 0,
+    parts_used: 0,
+    total_customers: 0,
+    vehicleEntriestats: [0, 0, 0, 0, 0],
+  }
+  data.delivered =
+    (await vehicleEntryModel.countDocuments({
+      outime: { $gte: startOfDate.toISOString() },
+    })) || 0
   data.in_garage =
-    (await vehicleEntryModel.countDocuments({ $and: [{ createdAt: { $gte: startOfToday.toISOString() } }, { outime: '' }] })) || 0
+    (await vehicleEntryModel.countDocuments({
+      $and: [{ createdAt: { $gte: startOfDate.toISOString() } }, { outime: '' }],
+    })) || 0
   data.parts_used = 25
-  data.total_customers = (await vehicleEntryModel.countDocuments({ createdAt: { $gte: startOfToday.toISOString() } })) || 0
-  data.visitorStats = visitorsByDay
+  data.total_customers =
+    (await vehicleEntryModel.countDocuments({
+      createdAt: { $gte: startOfDate.toISOString() },
+    })) || 0
+  data.vehicleEntriestats = vehicleEntriesByDay
 
   return data
 }
